83858dcc4c32995d6a9f87d927cf5ecfe3de71d4
atnabon <atnabond@gmail.com>
2024-03-12 16:18:43 -0500

    fix: scan tag

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import NfcManager, { NfcEvents, Ndef } from "react-native-nfc-manager"; // Import Ndef from react-native-nfc-manager
 
-export default function App() {
+const App = () => {
+  const [hasNfc, setHasNFC] = useState(null);
+
+  useEffect(() => {
+    NfcManager.setEventListener(NfcEvents.DiscoverTag, (tag) => {
+      console.log("tag found");
+    });
+
+    return () => {
+      NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
+    };
+  }, []);
+
+  const readTag = async () => {
+    await NfcManager.registerTagEvent();
+  };
+
+  const writeNFC = async () => {
+    let result = false;
+
+    try {
+      await NfcManager.requestTechnology(NfcTech.Ndef); // Import NfcTech from react-native-nfc-manager
+
+      const bytes = Ndef.encodeMessage([
+        Ndef.uriRecord("https://blog.logrocket.com/"),
+      ]);
+
+      if (bytes) {
+        await NfcManager.ndefHandler.writeNdefMessage(bytes);
+        result = true;
+      }
+    } catch (ex) {
+      console.warn(ex);
+    } finally {
+      NfcManager.cancelTechnologyRequest();
+    }
+
+    return result;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <TouchableOpacity onPress={readTag}>
+        <Text style={{ color: "blue" }}>Scan Tag</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={writeNFC}>
+        <Text style={{ color: "blue" }}>Write Tag</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
